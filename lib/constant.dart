6265c5fac0f789bcc2e128a7a436bfa2af4c378a
Denis John <denis.john.ndarake@gmail.com>
2019-08-12 23:45:23 +0100

    first commit with homepage header complete

---
+++ lib/constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kHeaderTextStyle = TextStyle(
+  fontSize: 42.0,
+  fontWeight: FontWeight.bold,
+);
---
+++ lib/src/widgets/home_top_info.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/constant.dart';
+
+class HomeTopInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "What would",
+              style: kHeaderTextStyle,
+            ),
+            Text(
+              "You like to eat?",
+              style: kHeaderTextStyle,
+            ),
+          ],
+        ),
+        Icon(
+          Icons.notifications_none,
+          size: 40,
+          color: Theme.of(context).primaryColor,
+        ),
+      ],
+    );
+  }
+}
