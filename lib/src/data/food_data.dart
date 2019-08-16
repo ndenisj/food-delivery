c1de31d87902899be05980ad9955f080b3ea1a8a
Denis John <denis.john.ndarake@gmail.com>
2019-08-17 00:00:03 +0100

    added new pages and main screen with a Tab navigation Bottom Bar

--- lib/src/data/food_data.dart
+++ lib/src/data/food_data.dart
@@ -1,21 +1,4 @@
-class Food {
-  final String id;
-  final String imagePath;
-  final String category;
-  final String name;
-  final double price;
-  final double discount;
-  final double ratings;
-
-  Food(
-      {this.id,
-      this.name,
-      this.imagePath,
-      this.category,
-      this.discount,
-      this.price,
-      this.ratings});
-}
+import '../models/food_model.dart';
 
 final foods = [
   Food(
---
+++ lib/src/models/food_model.dart
@@ -0,0 +1,18 @@
+class Food {
+  final String id;
+  final String imagePath;
+  final String category;
+  final String name;
+  final double price;
+  final double discount;
+  final double ratings;
+
+  Food(
+      {this.id,
+      this.name,
+      this.imagePath,
+      this.category,
+      this.discount,
+      this.price,
+      this.ratings});
+}
